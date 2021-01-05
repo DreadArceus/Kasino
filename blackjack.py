56b365723879701a76d0a98b8a1ceceee35da8fa
arcinston <69193694+arcinston@users.noreply.github.com>
2021-01-05 12:29:44 +0530

    blackjackv0.2

--- blackjack.py
+++ blackjack.py
@@ -1,21 +1,43 @@
 import discord
+import random
 from discord.ext import commands
 
-client =  commands.Bot(command_prefix=">")
+class Blackjack:
+    def __init__(self,player1,player2,rand_count):
+        self.player1=player1
+        self.player2=player2
+        self.rand_count=rand_count
+    
 
-player1_count=0
-dealer_count=0
-rand_count=0;
+client =  commands.Bot(command_prefix=">")
+switch = True
 @client.event
 async def on_ready():
     print("IM ready bitch")
-@client.commant()
-async def hit(ctx)
+@client.command()
+async def hitp1(ctx):
+    player1_count = 0
     await ctx.send("You have decided to hit")
+    await ctx.send(f"{player1_count} is the amt")
     rand_count=random.randint(1,10)
     player1_count+=rand_count
     if(player1_count>21):
-        player1_count=0
-        await.ctx.send("You have exceeded 21 and have lost \n TAke ThE L")
-    
+        await ctx.send("You have exceeded 21 and have lost \n TAke ThE L")
+        switch=False
+    else: 
+        await ctx.send(f"{player1_count} is the current number")
+        switch = True
+@client.command()
+async def hitp2(ctx):
+    player2_count = 0
+    await ctx.send("You have decided to hit")
+    rand_count=random.randint(1,10)
+    player2_count+=rand_count
+    if(player2_count>21):
+        await ctx.send("You have exceeded 21 and have lost \n TAke ThE L")
+
+client.run('NzgyMTY0NDkyMjc2NTk2NzU3.X8INcg.fZrEL4I3_FiWBGjz_v-VtLH9mQg')
+
+
+
 
