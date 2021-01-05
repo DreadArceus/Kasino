568f104967907b5c31655e37f4e8e487cb4f468a
ananjan <75256753+ananjan@users.noreply.github.com>
2021-01-05 09:59:48 +0530

    Update main.py (#10)
    Added grid and eastereggs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ custom = commands.Bot(command_prefix='!')
 coins = ['ez']  # here are the coins
 c = ['h', 't']
 t = ['following blocks contain player names and their ids is the index ']
+grid=['1','2','3']
 
 
 @custom.command()
@@ -15,23 +16,26 @@ async def h(ctx):
     await ctx.send('!p "name" /to play  ')
     await ctx.send('!b "your id" /shows you your balance  ')
     await ctx.send('!f "number of coins" "h/t" "id" /creates a coinflip  ')
+    await ctx.send('!g "number of coins" "column" "row" "id"/grid guess  ')
     await ctx.send('!i  /shows player names and their ids  ')
     await ctx.send('Rules:Everyone starts with a balance of 300 and first to 1000 coins wins')
+    await ctx.send('if you get addicted to gambling and lose your shit contact @DreadArceus')
+
 
 
 @custom.command()
-async def p(ctx, arg1):
-    temp1 = str(arg1)
-    temp2 = 300  # coins
-    if int(temp2) >= 0:
-
-        coins.append(temp2)
-        t.append(temp1)
-        i = t.index(temp1)
-        await ctx.send(arg1 + '  has been added with ' + '300 coins' + ' your id is ' + str(i))
+async  def p(ctx,arg1):
+    temp1=str(arg1)
+    temprich='rich'
+    if temprich in arg1 :
+     temp2=500 #coins rich mode
     else:
+     temp2=300 #coins
 
-        await ctx.send('you really thought that would work?')
+    coins.append(temp2)
+    t.append(temp1)
+    i=t.index(temp1)
+    await ctx.send(arg1  + '  has been added with ' +str(temp2) +' coins, your id is ' + str(i) )
 
 
 @custom.command()
@@ -60,6 +64,44 @@ async def f(ctx, arg1, arg2, arg3):  # arg1=coins arg3=id
 
     else:
         await ctx.send('you really thought that would work?')
+
+@custom.command()
+async  def g(ctx,arg11,arg22,arg33,arg44): #arg1=coins arg2=row arg3=column arg4=id
+         tempc=random.choice(grid)
+         tempr=random.choice(grid)
+         if (int(coins[int(arg44)]) - int(arg11)) >= 0:
+        
+    
+  
+
+
+           if (tempr==arg22) and (tempc==arg33):
+                temp44='you correctly guessed the grid location, coins are added to your balance'
+                outcome=9*int(arg11)
+           
+               
+     
+
+
+           else:
+                temp44='scammed'
+                outcome=-int(arg11)
+           temp55=int(coins[int(arg44)])
+           temp66= (temp55) + int(outcome) #temp 5 is updated coin values
+           if temp66 >=1000:
+                    await ctx.send(t[int(arg44)] + ' wins the session')
+           elif temp66==0:
+                    await ctx.send(t[int(arg44)] + ' is out')
+           else:
+                    await ctx.send(t[int(arg44)] + ' your balance has been updated, the correct grid postion was '+tempc +tempr)
+
+
+           coins[int(arg44)]=int(temp66)
+           await ctx.send(temp44)
+
+         else: 
+           await ctx.send('you really thought that would work?')
+
 
 
 @custom.command()
